79a9fac5a020f479b61c258240af2d25db852f33
Stephan Schmidt <schst@php.net>
2005-01-04 20:10:18 +0000

    added GetCategory2CS()
    git-svn-id: http://svn.php.net/repository/pear/packages/Services_Ebay/trunk@176464 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ Ebay/Call/GetCategory2CS.php
@@ -0,0 +1,57 @@
+<?PHP
+/**
+ * Get category 2 cs
+ *
+ * $Id$
+ *
+ * @package Services_Ebay
+ * @author  Stephan Schmidt <schst@php.net>
+ * @link    http://developer.ebay.com/DevZone/docs/API_Doc/Functions/GetCategory2CS/GetCategory2CSLogic.htm
+ *
+ * @todo    finish this API call
+ * @todo    build a model for this
+ */
+class Services_Ebay_Call_GetCategory2CS extends Services_Ebay_Call 
+{
+   /**
+    * verb of the API call
+    *
+    * @var  string
+    */
+    protected $verb = 'GetCategory2CS';
+
+   /**
+    * parameter map that is used, when scalar parameters are passed
+    *
+    * @var  array
+    */
+    protected $paramMap = array(
+                                 'CategoryId',
+                                );
+    
+   /**
+    * arguments of the call
+    *
+    * @var  array
+    */
+    protected $args = array(
+                            'DetailLevel' => 1
+                        );
+
+    protected $unserializerOptions = array(
+                                            'keyAttribute' => array('Category' => 'id')
+                                        );
+
+   /**
+    * make the API call
+    *
+    * @param    object Services_Ebay_Session
+    * @return   string
+    */
+    public function call(Services_Ebay_Session $session)
+    {
+        $return = parent::call($session);
+        return $return['Category2CS'];
+    }
+}
+?>
---
+++ examples/example_GetCategory2Cs.php
@@ -0,0 +1,22 @@
+<?PHP
+/**
+ * example that fetches categories
+ *
+ * $Id$
+ *
+ * @package     Services_Ebay
+ * @subpackage  Examples
+ * @author      Stephan Schmidt
+ */
+error_reporting(E_ALL);
+require_once '../Ebay.php';
+require_once 'config.php';
+
+$session = Services_Ebay::getSession($devId, $appId, $certId);
+
+$session->setToken($token);
+
+$ebay = new Services_Ebay($session);
+
+$cats = $ebay->GetCategory2CS();
+?>
